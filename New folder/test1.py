70f1ab66e79b21a1b0acd6100c3b4a9cb1adf477
aihm12 <152840188+aihm12@users.noreply.github.com>
2023-12-25 08:42:19 +0300

    Add files via upload

---
+++ New folder/test1.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 12 16:32:06 2023
+
+@author: admin
+"""
+
+'''
+x=5
+y=4
+z=x+y
+print (z)
+'''
+
+'''
+print("hello group")
+'''
+
+'''
+x="hello"
+y=5
+x=True
+b='a'
+'''
+'''
+x=input("enter the number")
+if x>20:
+    print("bigger than 10")
+else :
+    print("lower than 10")
+'''
+'''
+for i in range(0,10):
+    print(i)
+    '''
+
+'''
+for i in range(0,10):
+    for j in range(0,10):
+        print(i,j)
+      '''
+'''
+x=0
+while(x<10):
+    print(x)
+'''
+'''
+class a:
+  def __init__(self,n1,n2):
+    self.n1=n1
+    self.n2=n2      
+        
+  def sum(self):
+       return self.n1+self.n2
+   
+x=a(5,3)
+
+print(x.sum())
+'''
+'''
+x=int(input("n1"))
+y=int(input("n2"))
+
+z=x//0
+print(z)
+'''
+'''
+from math import *
+print((9+3)*5/4)
+
+'''
+'''
+squares = [x**2 for x in range(5)]
+print("التربيعات:", squares)
+'''
+'''
+sentence = "أنا أتعلم بايثون"
+print("عدد الحروف في الجملة:", len(sentence))
+'''
